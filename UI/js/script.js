2c2d9e789592e157c7acc549adb0c739f0c395f7
unknown <jamesugbanu@gmail.com>
2019-03-24 08:22:36 +0100

    Add Side navigation with mobile responsiveness

--- UI/js/script.js
+++ UI/js/script.js
@@ -1,13 +1,26 @@
 document.addEventListener('DOMContentLoaded', () => {
 	function toggleAccountOnWideScreen() {
-		const accountDiv = document.querySelector('#topBarHeaderAccount')
+		const profileDiv = document.querySelector('#topBarHeaderAccount')
+		const showMenu = document.querySelector('.header__hamburger')
 		const toggle = document.querySelector('.submenu')
+		const sidebar = document.querySelector('.mobile-sidebar')
+		const sidebar2 = document.querySelector('.sidebar__fixed--vertical')
 
-		accountDiv.addEventListener('click', () => {
+
+		profileDiv.addEventListener('click', () => {
 			if(toggle.classList.contains("show")) {
 				toggle.classList.remove("show")
 			} else {
 				toggle.classList.add("show")
+			}
+
+		})
+
+		showMenu.addEventListener('click', () => {
+		
+			if(sidebar.classList.contains("show")) {
+				sidebar2.classList.remove("false")
+				sidebar.classList.remove("show")	
 			}
 
 		})
