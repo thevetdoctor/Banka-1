45b96bc48d31f710a0c2ccfb81f0c3164f69f900
unknown <jamesugbanu@gmail.com>
2019-03-31 17:07:46 +0100

    add search function in account number input

--- UI/js/script.js
+++ UI/js/script.js
@@ -1,6 +1,23 @@
+const accountInfo = [
+		{
+			"id": 1,
+			"account_name": "James Joseph",
+			"account_number": "1234567890"
+		},
+		{
+			"id": 2,
+			"account_name": "Tunde Chukwuma",
+			"account_number": "1234567891"
+		},
+		{
+			"id": 3,
+			"account_name": "Chioma Musa",
+			"account_number": "1234567892"
+		}
+		]
+
 document.addEventListener('DOMContentLoaded', () => {
-	function toggleAccountOnWideScreen() {
-		const profileDiv = document.querySelector('#ProfileDropDown')
+	const profileDiv = document.querySelector('#ProfileDropDown')
 		const registerButton = document.querySelector('#RegisterOverlay')
 		const loginButton = document.querySelector('#LoginOverlay')
 		const showMenu = document.querySelector('.header__hamburger')
@@ -12,7 +29,7 @@ document.addEventListener('DOMContentLoaded', () => {
 		const registerOverlay = document.querySelector('.register__account_modal')
 		const loginOverlay = document.querySelector('.login__account_modal')
 
-
+	function toggleAccountOnWideScreen() {
 		profileDiv.addEventListener('click', () => {
 			if(toggle.classList.contains("show")) {
 				toggle.classList.remove("show")
@@ -58,5 +75,42 @@ document.addEventListener('DOMContentLoaded', () => {
 
 		})
 	}
+
+	 var account_input = document.querySelector('#account_input')
+
+    // Autocomplete for form
+	function filterSearch(event) {
+
+    // retireve the input element
+    let input = event.target;
+
+    // retrieve the datalist element
+    let account_list = document.querySelector('#account_list');
+
+    // minimum number of characters before we start to generate suggestions
+    var min_characters = 8;
+
+    if (input.value.length < min_characters ) { 
+        return;
+    } else { 
+
+    	 // clear any previously loaded options in the datalist
+                account_list.innerHTML = ""
+
+    	accountInfo.forEach(account => {
+    					if(account.account_number.includes(input.value)) {
+    						 // Create a new <option> element.
+			                    var option = document.createElement('option')
+			                    option.value = account.account_number + " - "+account.account_name
+								// attach the option to the datalist element
+			                    account_list.appendChild(option);
+    					}			
+					 
+				})
+       
+    }
+}
+
+	account_input.addEventListener("keyup", function(event){filterSearch(event)})
 	toggleAccountOnWideScreen()
 })
