0c4b232b7b1b11568eb300ccbf990aed67576468
unknown <jamesugbanu@gmail.com>
2019-05-20 08:28:47 +0100

    refactoring UI javascript

--- UI/js/script.js
+++ UI/js/script.js
@@ -6,7 +6,6 @@ document.addEventListener('DOMContentLoaded', () => {
   const secondNav = document.querySelector('.account__side__wrapper');
   const menu = document.querySelector('#st-container');
 
-
   const toggleClickedItem = ({ target }) => {
     if (target.classList.contains('circle')) toggle.classList.toggle('show');
     if (target.classList.contains('btn__register')) {
@@ -20,7 +19,10 @@ document.addEventListener('DOMContentLoaded', () => {
     if (target.classList.contains('navbar__text') || target.classList.contains('navbar__icon')) toggle.classList.toggle('show');
     if (target.classList.contains('register__modal')) registerOverlay.classList.add('show');
     if (target.classList.contains('login__modal')) loginOverlay.classList.add('show');
-    if (target.classList.contains('fa-dashboard') || target.classList.contains('fa-book')) secondNav.classList.toggle('show');
+    if (target.classList.contains('fa-dashboard') || target.classList.contains('fa-book')) {
+      secondNav.classList.toggle('show');
+    } else if (secondNav && !secondNav.classList.contains('show')) secondNav.classList.add('show');
+
     if (target.classList.contains('fa-bars')) {
       menu.classList.add('st-effect-1');
       menu.classList.add('st-menu-open');
